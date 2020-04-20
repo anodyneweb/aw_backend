09744e781e8a73d90cca0c14bf858db7f7d9bb86
sagarsharma89 <ersagarsharma89@gmail.com>
2020-04-20 21:30:33 +0530

    Front End: Added Email configuration in settings

--- anodyne/anodyne/settings.py
+++ anodyne/anodyne/settings.py
@@ -149,9 +149,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'vepolink@anodyne.in'
+EMAIL_HOST_PASSWORD = 'awecdelhi340@'
 APP_NAME = 'VepoLink'
+
 
 # Logging Setup
 # can set environ LOGLEVEL=debug/info/error etc
