62c0b9d93506ad753bebfab6eb5db256f10b1369
Sharma5 <ASharma5@ad.harman.com>
2021-03-02 11:12:53 +0530

    HSPCB data transmission update

--- anodyne/anodyne/PCBS/HSPCB.py
+++ anodyne/anodyne/PCBS/HSPCB.py
@@ -4,21 +4,19 @@ import requests
 import logging
 
 from api import utils
-from api.models import Parameter
+from api.models import Parameter, Station
 
 log = logging.getLogger('vepolink')
 
 
 class Handle:
     def __init__(self, **kwargs):
-        self.site = kwargs.get('station')
-        self.params = kwargs.get('params')
+        self.site = Station.objects.get(prefix=kwargs.get('prefix'))
         self.readings = kwargs.get('readings')
-        self.key = self.key
+        self.key = 'MDgwMzIwMTlfYW5vZHluZV93YXRlcl9lbmdpbmVlcmluZ19jb19wdnRfbHRkXzE2MTMyMA=='
         industry_id = self.site.industry.industry_id
-        self.URL = 'http://164.100.160.248/hrcpcb-api/api/industry/' \
-                   '{industryId}/station/{stationId}/data'.format(
-            industryId=industry_id, stationId=self.site.site_id)
+        self.URL = 'http://164.100.160.248/hrcpcb-api/api/industry/{industryId}/station/{stationId}/data'.format(industryId=industry_id, stationId=self.site.site_id)
+        self.deviceId = 'Anodyne_466'
 
     def get_headers(self):
         headers = {
@@ -28,10 +26,11 @@ class Handle:
         return headers
 
     def upload(self):
+        log.info('Initiating data uploading')
         status = requests.post(
             self.URL,
             headers=self.get_headers(),
-            data=self._get_data()
+            data=self.get_data()
         )
         log.info('HSPCB Upload Status :%s' % status)
         return status
@@ -73,3 +72,30 @@ class Handle:
         log.info('HSPCB URL:%s' % self.URL)
         log.info('HSPCB DATA:%s' % data)
         return json.dumps(data)
+
+    def get_data(self):
+        if self.readings:
+            for param, value in self.readings.items():
+                if param != 'timestamp':
+                    reading = value
+
+                    ts = self.readings.get('timestamp')
+                    ts = str(ts)
+                    ts = utils.epoch_timestamp(ts)
+                    ts = ts * 1000
+                    try:
+                        tmp_data = [{
+                            "params": [{
+                                "flag": "U",
+                                "parameter": "Flow",
+                                "timestamp": str(ts),
+                                "unit": "m3/hr",
+                                "value": reading
+                            }],
+                            "deviceId": self.deviceId,
+                        }]
+                    except Exception as err:
+                        log.exception('HSPCB Upload failure:%s' % err)
+
+        log.info('HSPCB DATA:%s' % json.dumps(tmp_data))
+        return json.dumps(tmp_data)
